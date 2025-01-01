6dc6bf1454012d0feb00c1c90f77a6afea2f24ba
ljjnardi524 <ljjnardi524@gmail.com>
2024-12-31 17:21:54 -0700

    Update navbar.jsx

--- src/components/main/Header/Navbar/navbar.jsx
+++ src/components/main/Header/Navbar/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="navbar__logo"><div className='navbar-logo-first'>[DIGI </div><div className='navbar-logo-second'> bujo</div><div className='navbar-logo-first'>]</div></div>
       <ul className="navbar__links">
         <li className="navbar__item"><a href="/home">Home</a></li>
-        <li className="navbar__item"><a href="https://www.google.com">Get Started!</a></li>
+        <li className="navbar__item"><a href="https://www.amazon.com/dp/B0DRXTFK9Z">Get Started!</a></li>
         {/*<li className="navbar__item"><a href="/nosotros">About Us</a></li>
         <li className="navbar__item"><a href="/servicios">Services</a></li>
         <li className="navbar__item"><a href="/contacto">Contact Us</a></li>*/}
